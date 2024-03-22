e64ed6ed6fad4c2d922b118e7bfd9fd7f13ca276
lukman <lukman@lukmans-MacBook-Air.local>
2024-03-22 15:27:26 +0800

    style : update modules

--- modules/profile/component/profile_v.tsx
+++ modules/profile/component/profile_v.tsx
@@ -2,13 +2,14 @@
 import {
   Box,
   ButtonIcon,
+  Center,
   EditIcon,
   HStack,
   Icon,
   Text,
 } from '@gluestack-ui/themed';
 import React from 'react';
-import {Image, TouchableOpacity, View} from 'react-native';
+import {Image, ScrollView, TouchableOpacity, View} from 'react-native';
 import ViewKartuPelanggan from './view_kartu_pelanggan';
 import {BadgeCheck, Gem, Smile, UserSearch, Users} from 'lucide-react-native';
 
@@ -16,127 +17,134 @@ export default function ProfileScreen({navigation}: any) {
   return (
     <>
       <Box bgColor="$fuchsia200" h={'$full'}>
-        <Box pl={'$4'} pr={'$4'}>
-          <HStack space="lg" pt={10}>
-            <Box>
-              <TouchableOpacity
-                onPress={() => navigation.navigate('InfoProfile')}>
-                <Image
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Box pl={'$4'} pr={'$4'}>
+            <Center>
+              <Box>
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('InfoProfile')}>
+                  <Image
+                    style={{
+                      width: 110,
+                      height: 110,
+                      borderRadius: 110 / 2,
+                    }}
+                    source={{uri: 'https://i.pravatar.cc/1000?img=3'}}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
                   style={{
-                    width: 110,
-                    height: 110,
-                    borderRadius: 30 / 2,
+                    width: 30,
+                    height: 30,
+                    borderRadius: 50 / 2,
+                    backgroundColor: '#FFFFFF',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    position: 'absolute',
+                    // right: 10,
+                    top: 80,
+                    left: 75,
                   }}
-                  source={{uri: 'https://i.pravatar.cc/1000?img=3'}}
-                />
-              </TouchableOpacity>
-              <View
-                style={{
-                  width: 30,
-                  height: 30,
-                  borderRadius: 50 / 2,
-                  backgroundColor: '#B347B5',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  position: 'absolute',
-                  // right: 10,
-                  top: 75,
-                  left: 75,
-                }}>
-                <ButtonIcon as={EditIcon} color="$white" />
-              </View>
+                  onPress={() => navigation.navigate('EditInfo')}>
+                  <ButtonIcon as={EditIcon} color="$fuchsia800" />
+                </TouchableOpacity>
+              </Box>
+            </Center>
+            <Center pt={10}>
+              <HStack space="md">
+                <Text fontSize={16} fontWeight="$bold" color="$black">
+                  INDRA PANGESTU
+                </Text>
+                <Icon as={BadgeCheck} size="lg" mt={1} color="blue" />
+              </HStack>
+            </Center>
+            <Box pt={25}>
+              <ViewKartuPelanggan />
             </Box>
-            <Text fontSize={16} fontWeight="$bold" color="$black">
-              INDRA PANGESTU
-            </Text>
-            <Icon as={BadgeCheck} size="lg" mt={1} color="blue" />
-          </HStack>
-          <Box pt={25}>
-            <ViewKartuPelanggan />
-          </Box>
-          <Box pt={25}>
-            <Box>
-              <Text fontWeight="$bold" color="$black" pb={10}>
-                Layanan lainnya
-              </Text>
-              <Box bg="#BBBBBB" style={{borderRadius: 20 / 2}} p={20}>
-                <TouchableOpacity
-                  onPress={() => navigation.navigate('Disukai')}>
-                  <HStack alignItems="center" space="md" mb={20}>
-                    <Box bg="$fuchsia700" p={15} borderRadius={10}>
-                      <Icon as={Smile} size="xl" mt={1} color="white" />
-                    </Box>
-                    <Box>
-                      <Text
-                        fontSize={14}
-                        textAlign="center"
-                        fontWeight="$bold"
-                        color="#030303">
-                        Lihat Siapa Suka Saya
-                      </Text>
-                      <HStack space="xs" alignItems="center" pt={3}>
-                        <Text fontSize={11} color="$fuchsia700">
-                          159
+            <Box pt={25}>
+              <Box>
+                <Text fontWeight="$bold" color="$black" pb={10}>
+                  Layanan lainnya
+                </Text>
+                <Box bg="#BBBBBB" style={{borderRadius: 20 / 2}} p={20}>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('Disukai')}>
+                    <HStack alignItems="center" space="md" mb={20}>
+                      <Box bg="$fuchsia700" p={15} borderRadius={10}>
+                        <Icon as={Smile} size="xl" mt={1} color="white" />
+                      </Box>
+                      <Box>
+                        <Text
+                          fontSize={14}
+                          textAlign="center"
+                          fontWeight="$bold"
+                          color="#030303">
+                          Lihat Siapa Suka Saya
                         </Text>
-                        <Text fontSize={11}>orang menyukai Anda</Text>
-                      </HStack>
-                    </Box>
-                  </HStack>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => navigation.navigate('Pengunjung')}>
-                  <HStack alignItems="center" space="md" mb={20}>
-                    <Box bg="$fuchsia700" p={15} borderRadius={10}>
-                      <Icon as={UserSearch} size="xl" mt={1} color="white" />
-                    </Box>
-                    <Box>
-                      <Text fontSize={14} fontWeight="$bold" color="#030303">
-                        Siapa Mengunjungi Saya
-                      </Text>
-                      <Text fontSize={11} pt={3}>
-                        Lihat siapa yang mengunjungi profil Anda
-                      </Text>
-                    </Box>
-                  </HStack>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => navigation.navigate('SayaSuka')}>
-                  <HStack alignItems="center" space="md" mb={20}>
-                    <Box bg="$fuchsia700" p={15} borderRadius={10}>
-                      <Icon as={Users} size="xl" mt={1} color="white" />
-                    </Box>
-                    <Box>
-                      <Text fontSize={14} fontWeight="$bold" color="#030303">
-                        Saya suka
-                      </Text>
-                      <Text pt={3} fontSize={11}>
-                        Tingkatkan Suka agar orang lain dapat
-                      </Text>
-                      <Text fontSize={11}>melihat Anda lebih cepat</Text>
-                    </Box>
-                  </HStack>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => navigation.navigate('Langganan')}>
-                  <HStack alignItems="center" space="md">
-                    <Box bg="$fuchsia700" p={15} borderRadius={10}>
-                      <Icon as={Gem} size="xl" mt={1} color="white" />
-                    </Box>
-                    <Box>
-                      <Text
-                        fontSize={14}
-                        textAlign="center"
-                        fontWeight="$bold"
-                        color="#030303">
-                        Langganan
-                      </Text>
-                    </Box>
-                  </HStack>
-                </TouchableOpacity>
+                        <HStack space="xs" alignItems="center" pt={3}>
+                          <Text fontSize={11} color="$fuchsia700">
+                            159
+                          </Text>
+                          <Text fontSize={11}>orang menyukai Anda</Text>
+                        </HStack>
+                      </Box>
+                    </HStack>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('Pengunjung')}>
+                    <HStack alignItems="center" space="md" mb={20}>
+                      <Box bg="$fuchsia700" p={15} borderRadius={10}>
+                        <Icon as={UserSearch} size="xl" mt={1} color="white" />
+                      </Box>
+                      <Box>
+                        <Text fontSize={14} fontWeight="$bold" color="#030303">
+                          Siapa Mengunjungi Saya
+                        </Text>
+                        <Text fontSize={11} pt={3}>
+                          Lihat siapa yang mengunjungi profil Anda
+                        </Text>
+                      </Box>
+                    </HStack>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('SayaSuka')}>
+                    <HStack alignItems="center" space="md" mb={20}>
+                      <Box bg="$fuchsia700" p={15} borderRadius={10}>
+                        <Icon as={Users} size="xl" mt={1} color="white" />
+                      </Box>
+                      <Box>
+                        <Text fontSize={14} fontWeight="$bold" color="#030303">
+                          Saya suka
+                        </Text>
+                        <Text pt={3} fontSize={11}>
+                          Tingkatkan Suka agar orang lain dapat
+                        </Text>
+                        <Text fontSize={11}>melihat Anda lebih cepat</Text>
+                      </Box>
+                    </HStack>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('Langganan')}>
+                    <HStack alignItems="center" space="md">
+                      <Box bg="$fuchsia700" p={15} borderRadius={10}>
+                        <Icon as={Gem} size="xl" mt={1} color="white" />
+                      </Box>
+                      <Box>
+                        <Text
+                          fontSize={14}
+                          textAlign="center"
+                          fontWeight="$bold"
+                          color="#030303">
+                          Langganan
+                        </Text>
+                      </Box>
+                    </HStack>
+                  </TouchableOpacity>
+                </Box>
               </Box>
             </Box>
           </Box>
-        </Box>
+        </ScrollView>
       </Box>
     </>
   );
--- modules/profile/index.ts
+++ modules/profile/index.ts
@@ -1,6 +1,7 @@
 import ProfileScreen from './component/profile_v';
 import SettingProfile from './component/setting_profile';
 import ViewDisukai from './component/view_disukai';
+import ViewEditInfo from './component/view_edit_info';
 import ViewInfoProfile from './component/view_info_profile';
 import ViewLangganan from './component/view_langganan';
 import ViewPengunjung from './component/view_pengunjung';
@@ -13,3 +14,4 @@ export {ViewDisukai};
 export {ViewLangganan};
 export {ViewPengunjung};
 export {ViewSayaSuka};
+export {ViewEditInfo};
